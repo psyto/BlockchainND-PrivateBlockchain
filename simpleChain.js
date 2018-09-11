5858f3f3e1a32088e97765e24654266ce895b5d4
Hiroyuki Saito <psyhiro@yahoo.co.jp>
2018-09-11 10:13:26 +0900

    cpied levelDB functions to simpleChain.js

--- simpleChain.js
+++ simpleChain.js
@@ -4,8 +4,13 @@
 
 const SHA256 = require('crypto-js/sha256');
 
-// SimpleChain.js includes the Node.js level library and configured to persist data within the project directory.
+/* ===== Persist data with LevelDB ===================================
+|  Learn more: level: https://github.com/Level/level     |
+|  =============================================================*/
 
+const level = require('level');
+const chainDB = './chaindata';
+const db = level(chainDB);
 
 /* ===== Block Class ==============================
 |  Class with a constructor for block 			   |
@@ -28,25 +33,29 @@ class Block{
 class Blockchain{
   constructor(){
 		// Genesis block persist as the first block in the blockchain using LevelDB.
-    this.chain = [];
-    this.addBlock(new Block("First block in the chain - Genesis block"));
+    // this.chain = [];
+    // this.addBlock(new Block("First block in the chain - Genesis block"));
+
   }
 
   // Add new block
 	// addBlock(newBlock) includes a method to store newBlock within LevelDB.
-  addBlock(newBlock){
-    // Block height
-    newBlock.height = this.chain.length;
+  async addBlock(newBlock){
+    // Previous block height
+		const previousBlockHeight = parseInt(await this.getBlockHeight());
+		// Block height
+		newBlock.height = previousBlockHeight + 1;
     // UTC timestamp
     newBlock.time = new Date().getTime().toString().slice(0,-3);
     // previous block hash
-    if(this.chain.length>0){
-      newBlock.previousBlockHash = this.chain[this.chain.length-1].hash;
+    if(newBlock.height>0){
+			const previousBlock = await this.getBlock(previousBlockHeight);
+      newBlock.previousBlockHash = previousBlock.hash;
     }
     // Block hash with SHA256 using newBlock and converting to a string
     newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
     // Adding block object to chain
-  	this.chain.push(newBlock);
+  	await this.addLevelDBData(newBlock.height, JSON.stringify(newBlock));
   }
 
   // Get block height
@@ -104,3 +113,50 @@ class Blockchain{
       }
     }
 }
+
+// Add data to levelDB with key/value pair
+function addLevelDBData(key,value){
+  db.put(key, value, function(err) {
+    if (err) return console.log('Block ' + key + ' submission failed', err);
+  })
+}
+
+// Get data from levelDB with key
+function getLevelDBData(key){
+  db.get(key, function(err, value) {
+    if (err) return console.log('Not found!', err);
+    console.log('Value = ' + value);
+  })
+}
+
+// Add data to levelDB with value
+function addDataToLevelDB(value) {
+    let i = 0;
+    db.createReadStream().on('data', function(data) {
+          i++;
+        }).on('error', function(err) {
+            return console.log('Unable to read data stream!', err)
+        }).on('close', function() {
+          console.log('Block #' + i);
+          addLevelDBData(i, value);
+        });
+}
+
+/* ===== Testing ==============================================================|
+|  - Self-invoking function to add blocks to chain                             |
+|  - Learn more:                                                               |
+|   https://scottiestech.info/2014/07/01/javascript-fun-looping-with-a-delay/  |
+|                                                                              |
+|  * 100 Milliseconds loop = 36,000 blocks per hour                            |
+|     (13.89 hours for 500,000 blocks)                                         |
+|    Bitcoin blockchain adds 8640 blocks per day                               |
+|     ( new block every 10 minutes )                                           |
+|  ===========================================================================*/
+
+
+(function theLoop (i) {
+  setTimeout(function () {
+    addDataToLevelDB('Testing data');
+    if (--i) theLoop(i);
+  }, 100);
+})(10);
